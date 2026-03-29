e6cbd2bcc955ace03684f7d60d4be52a13c075e8
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-03-29 17:22:53 +0545

    creating hero pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrepper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Link } from "lucide-react";
 
 export default function Home() {
   return (
-    <p>Welcome to Next.js! </p>
+    <>
+    <MaxWidthWrapper>
+      <div className='py-20 mx-auto text-center flex flex-col  items-center  max-w-3xl '>
+        <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>Your marketplace for high-quality{' '}<span className='text-blue-600'>digital assests </span>.</h1>
+        <p className='mt-6 text-lg max-w-prose text-muted-foreground '>Welcome to DigitalHippo. Every asset is verified by our team before being listed.</p>
+        <div className='flex flex-col sm:flex-row gap-4 mt-6'>
+          <Link href="/products" className={buttonVariants()}>Browse Trendings </Link>
+          <Button variant='ghost'>Our quality promise &rarr; </Button>
+        </div>
+      </div>
+      {/* TODO LIST PRODUCTS */}
+    </MaxWidthWrapper>
+    <section className="border-t border-gray-200 bg-gray-50">
+      <MaxWidthWrapper className="py-20">
+        <div className='grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0'></div>
+      </MaxWidthWrapper>
+    </section>
+    </>
   );
 }
+    
