944e0ce33c33fbe5813c81090fa22eff3cf5c850
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-03-29 14:50:05 +0545

    created a nextJs app and changed some css in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,13 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className= "h-full "
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={cn(" relative h-full font-sans  antialiased",geistSans.className)}>
+        <main className='relative flex flex-col min-h-screen'>
+         <div className='grow flex-1'> {children} </div>
+          </main>
+        </body>
     </html>
   );
-}
+} 
