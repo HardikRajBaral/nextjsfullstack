64f2a9b22c0bfcffc97fb0a2039c0812fc04ac96
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-03-31 22:03:28 +0545

    home page building in process with nav bar

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const PRODUCT_CATEGORIES=[
+    {
+        label:"UI kits", 
+        value:"ui-kits" as const,
+        featuered:[
+            {
+                name:"Editors picks",
+                href:"#",
+                imageSrc:"/nav/ui_kits/mixed.jpg"
+            }
+        ]
+    }
+]
